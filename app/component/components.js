3b23328b27c46c3d7bfcd122f2f2f4e8fdc9a865
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-05-04 12:09:19 +0530

    Added files via upload

---
+++ app/component/components.js
@@ -0,0 +1,122 @@
+//1.MainComponent Starts
+
+var MainComponent = React.createClass({
+
+	render: function()  {
+
+		return(
+         
+           <div>
+
+         <HeadComponent  />
+
+          <BodyComponent />
+
+            </div>
+
+			);
+
+	}
+
+
+});
+
+//1.MainComponent ends
+
+
+//1.1.HeadComponent Starts
+
+var HeadComponent = React.createClass({
+
+    render: function()  {
+
+       return(
+
+        <div>
+    
+        <TitleComponent />
+        <SearchComponent  />
+      
+
+        </div>
+
+
+       	);
+
+    }
+
+});
+
+//1.1.HeadComponents ends
+
+
+//1.1.1.TitleComponent Starts
+
+var TitleComponent = React.createClass({
+
+	render: function() {
+
+     return (
+
+          <h2>
+
+     Employee Attendence table
+
+          </h2>
+
+     	);
+
+	}
+
+
+
+
+});
+
+//1.1.1.TitleComponent ends
+
+
+//1.1.2.SearchComponent Starts
+
+var SearchComponent = React.createClass({
+
+	render: function() {
+
+     return (
+
+          <form>
+
+     <input type="text"  placeholder="Search for attendence" />
+
+          </form>
+
+     	);
+
+	}
+
+
+
+
+});
+
+
+//1.1.2.SearchComponent ends
+
+
+//1.2.BodyComponent Starts
+
+
+
+
+//1.2.BodyComponent ends
+
+
+
+ReactDom.render(
+
+<MainComponent  />,
+document.getElementById('app')
+
+	);
+
+
